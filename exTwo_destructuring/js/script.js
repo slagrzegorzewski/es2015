1f263d8ba074a973804698b0047e35921f4a5cc3
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-06-26 21:48:24 +0200

    ExerciseTwo - first look of destructuring

--- exTwo_destructuring/js/script.js
+++ exTwo_destructuring/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
-    console.log("hello");
+
+    var numbers = [1, 2, 3, 4, 5, 6],
+        [a, b, c] = numbers;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    var idCard = {
+        name: "Adam",
+        age: 30,
+        number: 4913
+    }
+
+    var {name, age, number} = idCard;
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+    console.log("Card number: " + number);
 });
