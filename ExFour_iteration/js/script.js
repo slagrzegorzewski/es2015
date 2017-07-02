096c81db07e42da1f0a70d99c95526b116223a98
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-07-02 13:17:23 +0200

    exFour - simple itertors

--- ExFour_iteration/js/script.js
+++ ExFour_iteration/js/script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-   console.log("test");
+    "use strict";
+    var table = ["Raptor", "T-rex", "Triceratops", "Compsognatus", "Apatosaurus"];
+
+    // wyoprowadzenie kluczy oraz ich wartości do konsoli
+    /*for (let key in table) {
+        console.log(key + " " + table[key]);
+    }*/
+    for (let value of table) {
+        console.log(value);
+    }
+
+    const myName = "Slawomir";
+    for (const char of myName){
+        console.log(char);
+    }
 });
