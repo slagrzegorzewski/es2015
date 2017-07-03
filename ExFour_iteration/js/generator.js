4453fe628bdca4b63e363c608a22499feb084d0f
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-07-03 23:36:59 +0200

    exFour - generator sample

--- ExFour_iteration/js/generator.js
+++ ExFour_iteration/js/generator.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
+    function *gen() {
+        yield "One";
+        yield "Two";
+        yield "Three";
+        yield "Four";
+    }
 
+    let x = gen();
+    console.log(x.next().value);
+    console.log(x.next().value);
+    console.log(x.next().value);
+    console.log(x);
+    console.log(x.next().value);
+    console.log(x.next().done);
+    console.log(x);
+
+    function *watches() {
+        yield "Atlantic";
+        yield "Casio";
+        yield "Timex";
+        yield "Daniel Wellington";
+    }
+
+    let myWatch = watches();
+
+    for(let watch of myWatch) {
+        console.log(watch);
+    }
 });
