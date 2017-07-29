f07edfb31b0749f397fdd0bafad9c8a7cdd8000a
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-07-29 23:20:46 +0200

    exFive - magical methods

--- exFive_Class/js/constructor.js
+++ exFive_Class/js/constructor.js
@@ -6,9 +6,27 @@ document.addEventListener("DOMContentLoaded", function () {
             this.id = id;
             this.name = name;
         }
+        getName() {
+            return this.name;
+        }
+        // up! this metod we are using on the object
         /*Remember Class can have only one constructor
         constructor(){}*/
+
+        static compareName(a, b){
+            const nameA = a.name;
+            const nameB = b.name;
+            if(nameA == nameB){
+                return "You have two dinos with different id number: " + a.id + " and " + b.id;
+            }
+            else {
+                return "You have two different dinos: " + nameA + " and " + nameB;
+            }
+        }
+        //up! this metod we are using on the class
     }
-    const dino = new Dinosaur(111, "Velociraptor");
-    console.log("Dino ID: " + dino.id + " " + "name: " + dino.name);
+    const dinoA = new Dinosaur(111, "Velociraptor");
+    const dinoB = new Dinosaur(123, "T-rex");
+    console.log("Dino ID: " + dinoA.id + " " + "name: " + dinoA.getName());
+    console.log(Dinosaur.compareName(dinoA, dinoB));
 });
