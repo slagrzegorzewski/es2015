a7437cda4ffed78b51a6a27eeee1f6584711a4d2
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-07-30 21:59:41 +0200

    exSix - first round with promise

--- exSix_promise/js/promise.js
+++ exSix_promise/js/promise.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
+
+    let x = 4;
+
+    let knock = new Promise(function(resolve, reject){
+        if(x == 1){
+            resolve("I`ve got it");
+        }
+        else{
+            reject(Error("Something goes wrong..."));
+        }
+    });
+    console.log(knock);
+
+    knock.then(function(result){
+        console.log(result);
+    }).catch(function(error){
+        console.log(error);
+    });
 });
