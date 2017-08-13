480b47e51a481d12c8728420bbab9482946b675d
Slawomir Grzegorzewski <slagrzegorzewski@gmail.com>
2017-08-13 22:23:56 +0200

    exEight - proxies examples

--- exEight_diffTech/src/proxies.js
+++ exEight_diffTech/src/proxies.js
@@ -5,4 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
         return "test";
     };
     console.log(test());
+
+    let x = {};
+    let handler = {
+        get: function(obj, key) {
+//            return obj[key] * 2
+            return `Preference key ${obj[key]}`
+        }
+    };
+    let n = new Proxy(x, handler);
+
+    n.id = 123;
+    n.title = "hello world";
+
+    console.log(n.title);
 });
